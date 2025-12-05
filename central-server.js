b15be6c852610a1e1514e410d75f287991a8555a
Jean Vega <jean.vega@sealabs.cr>
2025-12-05 08:13:14 -0600

    some validations

--- central-server.js
+++ central-server.js
@@ -146,7 +146,7 @@ io.on("connection", (socket) => {
             }
 
             TX.set(id, { id, fromBank: fromBankResult.bankId, from, to, toBank: toBankResult.bankId, amount, currency, state: "NEW" });
-
+            console.log("from", fromBankResult.bankId, "toBankResult:", toBankResult.bankId)
             // 2) CENTRAL -> ORIGEN: transfer.reserve (solicitar reserva)
             emitTo(fromBankResult.bankId, "transfer.reserve", { id });
             emitTo(toBankResult.bankId, "transfer.init", { id });
